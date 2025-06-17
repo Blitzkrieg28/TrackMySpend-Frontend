5caa6a211e7e7351c5efcb653ca62e42b4f38b67
Tanmay <tanatwork2809@gmail.com>
2025-06-17 22:18:12 +0530

    sign-in page done with routing

--- trackmyspend/src/pages/Signin.jsx
+++ trackmyspend/src/pages/Signin.jsx
@@ -1,3 +1,12 @@
+import Input from "../components/Input";
+
 export default function Signin(){
-    return <div>Signin</div>
+    return <div className="flex min-h-screen justify-center items-center bg-[#e8e8e8]">
+        <div className="flex flex-col justify-center items-center bg-white p-6 rounded-lg shadow-lg">
+            <h1 className="pb-6 text-4xl text-center">Sign-in</h1>
+            <div className="h-0.5 bg-[#e8e8e8] w-full mb-4"></div>
+           <Input/>
+
+        </div>
+    </div>
 }
